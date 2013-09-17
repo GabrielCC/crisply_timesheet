cca8e0958f3d712ccd37cbcf9e67ae7cde2fa151
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2013-09-18 00:54:53 +0300

    'testing'

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :crisply_timesheet do
+  name 'Crisply Timesheet plugin'
+  author 'Gabriel Croitoru'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  author_url 'http://gabrielcroitoru.ro'
+end
